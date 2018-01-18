fa343d021b4db1506dd3ed65081d256d0a996be1
rhino <ryan@perfectsensedigital.com>
2018-01-18 13:45:00 -0500

    Adds delete option to delete old files

--- deploy.sh
+++ deploy.sh
@@ -22,12 +22,12 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
 
         echo "Syncing release..."
         version=$(awk -F '/' '{print $2}' <<< $TRAVIS_BRANCH)
-        aws s3 sync $2 s3://$AWS_BUCKET/v$version --acl $AWS_ACL --cache-control max-age=3600
+        aws s3 sync $2 s3://$AWS_BUCKET/v$version --acl $AWS_ACL --cache-control max-age=3600 --delete
     fi
 
     if [[ "$TRAVIS_BRANCH" == "master" ]]; then
 
         echo "Syncing latest..."
-        aws s3 sync $2 s3://$AWS_BUCKET/ --acl $AWS_ACL --cache-control max-age=3600
+        aws s3 sync $2 s3://$AWS_BUCKET/ --acl $AWS_ACL --cache-control max-age=3600 --delete
     fi
 fi
