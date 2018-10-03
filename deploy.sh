e72ef3078a9f0fc6101845c5cd0ef4d46522af85
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2018-10-03 12:37:07 -0400

    Revert to 577afe4.

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,7 @@ sudo pip install -r requirements.txt
 cd $1
 make html
 
-# If this Travis job is not a pull request, 
-# assume it is a merge and deploy to S3
+#S3 Deploy
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
 
     sudo pip install awscli
@@ -29,20 +28,7 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
 
     if [[ "$TRAVIS_BRANCH" == "master" ]]; then
 
-      echo -n "Moving versioned topics to temporary directory..."
-      aws s3 mv s3://$AWS_BUCKET/ tmp/ --acl private --include "/v[3-9].[0-9]/*" --recursive 
-      echo "done"
-      tree -l
-
-      echo -n "Syncing latest..."
-      aws s3 sync $2 s3://$AWS_BUCKET/ --acl $AWS_ACL --cache-control max-age=3600 --delete
-      echo "done"
-
-      # echo -n "Moving versioned topics back to public bucket..."
-      # aws s3 mv tmp/ s3://$AWS_BUCKET/ --acl $AWS_ACL --cache-control max-age=3600 --include "/v[3-9].[0-9]/*" --recursive
-      # echo "done"
-
+        echo "Syncing latest..."
+        aws s3 sync $2 s3://$AWS_BUCKET/ --acl $AWS_ACL --cache-control max-age=3600 --delete
     fi
-else
-  echo "As this build was for Pull Request $TRAVIS_PULL_REQUEST, there was no deployment to AWS."
 fi
