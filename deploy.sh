186789a932b9e818605070bdc9cf246047b5d570
Mark Lautman <mlautman@perfectsensedigital.com>
2018-10-05 16:00:56 -0400

    Updated script to account for AWS includes/excludes.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-
+# In a test environment, run this script as follows:
+# 1) Change to the repo containing the documentation (cd ~/docs)
+# 2) Place this script inside the repo directory
+# 3) Run ./deploy.sh . _build/html
+#
+# Uncomment the following two lines for testing:
+# TRAVIS_PULL_REQUEST="false"
+# AWS_BUCKET=mldocs.brightspot.com
+#
+# Scenario 1: deploying a previous release. Uncomment the following:
+# TRAVIS_BRANCH=release/3.2
+# Scenario 2: deploying the current release. Uncomment the following:
+# TRAVIS_BRANCH=master
+#
 set -e -u
 
 # Sphinx Build
-#sudo pip install --upgrade pip
 sudo pip install --upgrade pip==9.0.3
 sudo pip install -r requirements.txt
 cd $1
@@ -11,35 +23,26 @@ make html
 
 # If this Travis job is not a pull request, 
 # assume it is a merge and deploy to S3
+
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
 
     sudo pip install awscli
 
-    AWS_ACL="${AWS_ACL-public-read}"
-
     echo "Deploying to bucket: $AWS_BUCKET"
-    echo "ACL: $AWS_ACL"
 
     if [[ "$TRAVIS_BRANCH" == "release/"* ]]; then
 
-        echo "Syncing release..."
         version=$(awk -F '/' '{print $2}' <<< $TRAVIS_BRANCH)
-        aws s3 sync $2 s3://$AWS_BUCKET/v$version --acl $AWS_ACL --cache-control max-age=3600 --delete
+        echo "Synching for release $version..."
+        aws s3 sync $2 s3://$AWS_BUCKET/v$version --acl public-read --cache-control max-age=3600 --delete
+        echo "Done synching release $version"
     fi
 
     if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-
-      # echo -n "Moving versioned topics to temporary directory..."
-      # aws s3 mv s3://$AWS_BUCKET/ tmp/ --acl private --include "/v[3-9].[0-9]/*" --recursive 
-      # echo "done"
     
-      echo -n "Syncing latest..."
-      aws s3 sync $2 s3://$AWS_BUCKET/ --acl $AWS_ACL --cache-control max-age=3600 --delete
-      echo "done"
-
-      # echo -n "Moving versioned topics back to public bucket..."
-      # aws s3 mv tmp/ s3://$AWS_BUCKET/ --acl $AWS_ACL --cache-control max-age=3600 --include "/v[3-9].[0-9]/*" --recursive
-      # echo "done"
+      echo "Synching master..."
+      aws s3 sync $2 s3://$AWS_BUCKET/ --acl public-read --include "*" --exclude "v?.?/*"  --delete
+      echo "Done syching master"
 
     fi
 else
