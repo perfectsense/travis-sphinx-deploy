b2575fecfd734bb9cb9314aabc582b94664956ec
Mark Lautman <mlautman@perfectsensedigital.com>
2019-04-09 15:48:56 -0400

    Only-binary for pip install.

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,8 @@ set -e -u
 # Sphinx Build
 sudo pip install --upgrade pip==9.0.3
 sudo pip install -r requirements.txt
-sudo pip install -i https://test.pypi.org/simple/ pygments-lexer-overrides==0.0.3
-python -m pygments-lexer-overrides
+sudo pip install -i https://test.pypi.org/simple/ --only-binary pygments-lexer-overrides==0.0.3
+sudo python -m pygments-lexer-overrides
 
 cd $1
 make html
