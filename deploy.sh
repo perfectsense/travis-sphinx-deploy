12a2b80bcb047bbfe18977d92b1c19ac247c58cc
Mark Lautman <mlautman@perfectsensedigital.com>
2019-04-09 15:13:42 -0400

    Run script to make symbolic links for lexers.

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,7 @@ set -e -u
 sudo pip install --upgrade pip==9.0.3
 sudo pip install -r requirements.txt
 sudo pip install -i https://test.pypi.org/simple/ pygments-lexer-overrides
+python -m pygments-lexer-overrides
 
 cd $1
 make html
